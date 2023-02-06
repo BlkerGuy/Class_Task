36bcb9f468fddcc0f6ffe1e0d1db149642154167
BlkerGuy <2020.gafarov@gmail.com>
2023-02-06 22:56:35 +0400

    Add project files.

---
+++ Class_Task/Notebooks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Task
+{
+    class Notebooks
+    {
+        public string Brand;
+        public string Model;
+        public int Price;
+
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"\n Brand:{Brand},\n Model:{Model},\n Price:{Price}");
+        }
+        public Notebooks()
+        {
+        }
+        public Notebooks(string brand,string model)
+        {
+            Brand = brand;
+            Model = model;
+
+        }
+        
+    }
+}
---
+++ Class_Task/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Class_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Task 1
+            Notebooks notebook = new Notebooks();
+            notebook.Brand = "ASUS";
+            notebook.Model = "TUF GAMING";
+            notebook.Price = 2000;
+
+            notebook.ShowInfo();
+            
+
+            // Task 2
+            Notebooks notebook2 = new Notebooks("ASUS","ROG STRIX");
+            notebook2.Price = 2099;
+            notebook2.ShowInfo();
+
+
+            // Task 3
+            Notebooks[] notebooks = new Notebooks[] { notebook, notebook2 };
+
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i <notebooks.Length; i++)
+            {
+                if (notebooks[i]!=null && notebooks[i].Price!=0)    //Burda null ve qiymetini 0 yoxlayiram null
+                {                                                   //olmasa'da qiymeti (default) teyin olunmamis ola biler.
+                    sum += notebooks[i].Price;
+                    count++;
+                }
+            }
+            Console.WriteLine("\nNotebooks Average:"+sum/count);
+            
+        }                                                   
+    }
+}
